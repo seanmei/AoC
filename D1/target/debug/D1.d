a1690ef6faaabdc0d61242a17be9506e1fdc2751
Sean <seanmei0728@gmail.com>
2022-12-29 17:15:01 -0500

    git commit

---
+++ D1/target/debug/D1.d
@@ -0,0 +1,1 @@
+/Users/seanmei/Projects/AoC/D1/target/debug/D1: /Users/seanmei/Projects/AoC/D1/src/main.rs
---
+++ D1/target/debug/deps/D1-2a474d5a6b32548c.d
@@ -0,0 +1,5 @@
+/Users/seanmei/Projects/AoC/D1/target/debug/deps/D1-2a474d5a6b32548c: src/main.rs
+
+/Users/seanmei/Projects/AoC/D1/target/debug/deps/D1-2a474d5a6b32548c.d: src/main.rs
+
+src/main.rs:
