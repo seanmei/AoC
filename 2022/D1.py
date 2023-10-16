430ed8a2603ab66706bce4304bf06811a26f6740
Sean <seanmei0728@gmail.com>
2023-10-15 23:20:04 -0400

    d1 complete

--- 2022/D1.py
+++ 2022/D1.py
@@ -1,10 +1,14 @@
-
-max = 0 #current highest
-
+max = [0,0,0] #current 3 highest
 
 sum = 0 #total for each elf 
 
+
 cals = open('input.txt', 'r')
+
+def calCheck(sum): 
+    max.sort()
+    if sum > max[0]:
+        max[0]=sum   
 
 while True:
 
@@ -13,11 +17,16 @@ while True:
     if not line:
         break
     if line == "\n":
-        if sum > max: #check for new highest for each elf 
-            max = sum
+        calCheck(sum)
         sum =0 #rest counter 
     else:
         sum += int(line)
+
 print(max)
+total = 0
+for x in max: 
+    total += x #total of all 3 elves 
+print(total)
+
     
     
