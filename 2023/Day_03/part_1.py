b30ede5af42338603104cc33771b60549273a052
Sean <seanmei0728@gmail.com>
2023-12-07 02:14:08 -0500

    Aoc Day 3 part 1 begin

---
+++ 2023/Day_03/part_1.py
@@ -0,0 +1,17 @@
+# read each line 
+
+#Case 1: symbol infront of number 
+#Case 2: symbol behind a number 
+#Case 3: symbol above a number 
+#Case 4: symbol below a number
+#Case 5: Diagonals which are below/above and +/- 1 to the left or right 
+
+sum = 0 
+
+
+#find each number in each line and get the index of the first and last digit 
+
+#check before and after for symbol
+
+#if not, get index and check the rows above and below and use the the index of the first and last digit +/- 1 for also account for diagonals 
+
